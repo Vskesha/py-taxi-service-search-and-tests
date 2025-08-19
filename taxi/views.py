3a6388b30cc21801262d069f52d5d1aaed2937d2
Vasyl Boliukh <vskesha@gmail.com>
2025-08-19 17:46:08 +0300

    fixed line length (flake8)

--- taxi/views.py
+++ taxi/views.py
@@ -124,7 +124,9 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         username = self.request.GET.get("username", "")
-        context["search_form"] = DriverSearchForm(initial={"username": username})
+        context["search_form"] = DriverSearchForm(
+            initial={"username": username}
+        )
         return context
 
     def get_queryset(self):
