55bf904bdcf2c4c9f3a72586282b0b8c95c7e007
Vasyl Boliukh <vskesha@gmail.com>
2025-08-20 08:04:26 +0300

    fixed problems with driver deletion and assigning to a nonexisting car

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -156,15 +156,14 @@ class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Driver
-    success_url = reverse_lazy("")
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 @login_required
 def toggle_assign_to_car(request, pk):
     driver = Driver.objects.get(id=request.user.id)
-    if (
-        Car.objects.get(id=pk) in driver.cars.all()
-    ):  # probably could check if car exists
+    car = get_object_or_404(Car, id=pk)
+    if car in driver.cars.all():
         driver.cars.remove(pk)
     else:
         driver.cars.add(pk)
